bc155110733581c35b52c51f3450028c02dd72ab
gangwarsaurabh63 <gangwarsaurabh63976@gmail.com>
2026-08-21 13:19:49 +0530

    Update physics.js

--- physics.js
+++ physics.js
@@ -1,9 +1,1 @@
-const physicsQuestions = [
-    {
-        subject: "Physics",
-        q: "A proton accelerated through a potential V has de-Broglie wavelength λ. Then the de-Broglie wavelength of an α-particle, when accelerated through the same potential V, is:",
-        options: ["λ / 2", "λ / √2", "λ / (2√2)", "λ / 8"],
-        correct: 2
-    },
-    // ... add up to 45 physics questions here
-];
+
